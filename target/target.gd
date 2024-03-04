4c71aadff6d63cbc6a9e01f30a560b9dea6a8a66
Sl0th <85708571+officialSl0th@users.noreply.github.com>
2024-03-04 17:21:06 +0100

    add target controller

--- target/target.gd
+++ target/target.gd
@@ -1,12 +1,13 @@
-extends Node2D
-class_name Target
+extends Node2D;
+class_name Target;
 
-func _ready() -> void:
-	pass;
-
+@export var radius: float;
+@export var color: Color;
 
 func _draw() -> void:
-	pass;
+	draw_circle(Vector2(0, 0), radius, color);
 
-func _process(delta) -> void:
-	pass;
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton && event.button_index == 1:
+		set_position(get_viewport().get_mouse_position());
