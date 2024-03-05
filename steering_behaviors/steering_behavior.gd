99ac438ee01ef5f2adb4012c5b5d86d7697e30e0
Sl0th <85708571+officialSl0th@users.noreply.github.com>
2024-03-05 17:17:36 +0100

    add pursuit

--- steering_behaviors/steering_behavior.gd
+++ steering_behaviors/steering_behavior.gd
@@ -6,6 +6,7 @@ enum SteeringBehaviorType {
 	SEEK,
 	FLEE,
 	ARRIVE,
+	PURSUIT,
 }
 
 enum Deceleration {
@@ -15,20 +16,23 @@ enum Deceleration {
 }
 
 ## Target the agent will use for force calculations
-@export var target: Node2D;
+@export var target_node: Node2D;
 
 var _type: SteeringBehaviorType;
-var _deceleration: Deceleration;
-var _deceleration_tweeker: float = 0.3;
+
+# flee
 var _panic_radius: float;
 var _show_panic_radius: bool;
 
+# arrive
+var _deceleration: Deceleration;
+var _deceleration_tweeker: float = 0.3;
+
+# pursuit
+var _predicted_position: Vector2;
+var _show_predicted_position: bool;
+
 @onready var _vehicle: Vehicle = get_parent();
-
-func _draw() -> void:
-	if _show_panic_radius:
-		draw_arc(Vector2.ZERO, _panic_radius, 0, 2 * PI, 360, Color(0, 0, 1, 1));
-
 
 func _get(property: StringName):
 	if property == "/type":
@@ -45,6 +49,9 @@ func _get(property: StringName):
 
 	if property == "/arrive/deceleration_tweeker":
 		return _deceleration_tweeker;
+
+	if property == "/pursuit/show_predicted_position":
+		return _show_predicted_position;
 
 
 func _set(property: StringName, value: Variant) -> bool:
@@ -63,6 +70,9 @@ func _set(property: StringName, value: Variant) -> bool:
 
 	if property == "/arrive/deceleration_tweeker":
 		_deceleration_tweeker = value;
+
+	if property == "/pursuit/show_predicted_position":
+		_show_predicted_position = value;
 
 	return true
 
@@ -102,36 +112,54 @@ func _get_property_list() -> Array[Dictionary]:
 			"type": TYPE_FLOAT,
 		})
 
+	if _type == SteeringBehaviorType.PURSUIT:
+		property_list.append({
+			"name": "/pursuit/show_predicted_position",
+			"type": TYPE_BOOL,
+		})
+
 	return property_list
 
 
-func get_show_panic_radius() -> bool:
-	return _show_panic_radius;
+func _draw() -> void:
+	if _show_panic_radius:
+		draw_arc(Vector2.ZERO, _panic_radius, 0, 2 * PI, 360, Color(0, 0, 1, 1));
+
+	if _show_predicted_position:
+		draw_circle(to_local(_predicted_position), 5, Color(0, 1, 0, 1));
+
+
+func _process(_delta: float) -> void:
+	if _show_panic_radius || _show_predicted_position:
+		queue_redraw();
 
 
 func calculate() -> Vector2:
 	match _type:
 		SteeringBehaviorType.SEEK:
-			return _seek();
+			return _seek(target_node.position);
 
 		SteeringBehaviorType.FLEE:
-			return _flee();
+			return _flee(target_node.position);
 
 		SteeringBehaviorType.ARRIVE:
-			return _arrive();
+			return _arrive(target_node.position);
+
+		SteeringBehaviorType.PURSUIT:
+			return _pursuit(target_node.position);
 
 		_:
 			return Vector2.ZERO;
 
 
-func _seek() -> Vector2:
-	var _desired_velocity: Vector2 = _vehicle.position.direction_to(target.position) * _vehicle.max_speed;
+func _seek(target: Vector2) -> Vector2:
+	var _desired_velocity: Vector2 = _vehicle.position.direction_to(target) * _vehicle.max_speed;
 	_vehicle.desired_velocity += _desired_velocity;
 	return _desired_velocity - _vehicle.velocity;
 
 
-func _flee() -> Vector2:
-	var _to_target: Vector2 = _vehicle.position - target.position;
+func _flee(target: Vector2) -> Vector2:
+	var _to_target: Vector2 = _vehicle.position - target;
 	var _distance: float = _to_target.length();
 
 	if _distance < _panic_radius || _panic_radius < 0:
@@ -142,8 +170,8 @@ func _flee() -> Vector2:
 	return Vector2.ZERO;
 
 
-func _arrive() -> Vector2:
-	var _to_target: Vector2 = target.position - _vehicle.position;
+func _arrive(target: Vector2) -> Vector2:
+	var _to_target: Vector2 = target - _vehicle.position;
 	var _distance: float = _to_target.length();
 
 	if _distance > 0:
@@ -156,3 +184,19 @@ func _arrive() -> Vector2:
 		return _desired_velocity - _vehicle.velocity;
 
 	return Vector2.ZERO;
+
+
+func _pursuit(target: Vector2) -> Vector2:
+	if !(target_node is Vehicle):
+		return _seek(target);
+
+	var _to_target: Vector2 = target - _vehicle.position;
+	var _relative_heading: float = _vehicle._heading.dot(target_node._heading);
+
+	if _to_target.dot(_vehicle._heading) > 0 && _relative_heading < -0.95:
+		return _seek(target);
+
+	var _look_ahead_time: float = _to_target.length() / (_vehicle.max_speed + target_node.velocity.length());
+	_predicted_position = target + target_node.velocity * _look_ahead_time;
+
+	return _seek(_predicted_position);
