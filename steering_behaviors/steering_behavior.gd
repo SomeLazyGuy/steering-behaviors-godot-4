c6c9fad084bb8d123d212f6901d5f60da8c228ec
Flootty <konstantin.kilbel@gmail.com>
2024-09-13 18:29:39 +0200

    add obstacle avoidance

--- steering_behaviors/steering_behavior.gd
+++ steering_behaviors/steering_behavior.gd
@@ -9,6 +9,7 @@ enum SteeringBehaviorType {
 	PURSUIT,
 	EVADE,
 	WANDER,
+	OBSTACLE_AVOIDANCE,
 }
 
 enum Deceleration {
@@ -34,12 +35,21 @@ var _deceleration_tweeker: float = 0.3;
 var _predicted_position: Vector2;
 var _show_predicted_position: bool;
 
+var _show_visuals: float;
+
 # wander
 var _wander_target: Vector2;
 var _wander_radius: float;
 var _wander_distance: float;
 var _wander_jitter: float;
-var _show_visuals: float;
+
+# obstacle avoidance
+var _min_detection_box_length: float;
+var _detection_box_length: float;
+var _area2d: Area2D;
+var _collision_shape2d: CollisionShape2D;
+var _rectangle_shape2d: RectangleShape2D;
+var _obstacles: Array[Obstacle];
 
 @onready var _vehicle: Vehicle = get_parent();
 
@@ -73,6 +83,15 @@ func _get(property: StringName):
 
 	if property == "/wander/show_visuals":
 		return _show_visuals;
+		
+	if property == "/wander/show_visuals":
+		return _show_visuals;
+
+	if property == "/obstacles/show_visuals":
+		return _show_visuals;
+		
+	if property == "/obstacles/min_detection_box_length":
+		return _min_detection_box_length;
 
 
 func _set(property: StringName, value: Variant) -> bool:
@@ -110,6 +129,12 @@ func _set(property: StringName, value: Variant) -> bool:
 
 	if property == "/wander/show_visuals":
 		_show_visuals = value;
+		
+	if property == "/obstacles/show_visuals":
+		_show_visuals = value;
+		
+	if property == "/obstacles/min_detection_box_length":
+		_min_detection_box_length = value;
 
 	return true
 
@@ -193,8 +218,34 @@ func _get_property_list() -> Array[Dictionary]:
 			"name": "/wander/show_visuals",
 			"type": TYPE_BOOL,
 		})
+		
+	if _type == SteeringBehaviorType.OBSTACLE_AVOIDANCE:
+		property_list.append({
+			"name": "/obstacles/show_visuals",
+			"type": TYPE_BOOL,
+		})
+		
+		property_list.append({
+			"name": "/obstacles/min_detection_box_length",
+			"type": TYPE_FLOAT,
+			"hint": PROPERTY_HINT_RANGE,
+			"hint_string": "0,200,or_greater",
+		})
 
 	return property_list
+
+
+func _ready() -> void:
+	if _type == SteeringBehaviorType.OBSTACLE_AVOIDANCE:
+		_rectangle_shape2d = RectangleShape2D.new();
+		_collision_shape2d = CollisionShape2D.new();
+		_collision_shape2d.shape = _rectangle_shape2d;
+		_area2d = Area2D.new();
+		_area2d.area_entered.connect(_obstacle_entered);
+		_area2d.area_exited.connect(_obstacle_exited);
+		
+		add_child(_area2d);
+		_area2d.add_child(_collision_shape2d);
 
 
 func _draw() -> void:
@@ -211,8 +262,13 @@ func _draw() -> void:
 		draw_circle(to_local(_predicted_position), 5, _color);
 
 	if _show_visuals:
-		draw_arc(Vector2.UP * _wander_distance, _wander_radius, 0, 2 * PI, 360, Color(0, 0, 1, 1));
-		draw_circle(_wander_target, 5, Color(0, 1, 0, 1));
+		if _type == SteeringBehaviorType.WANDER:
+			draw_arc(Vector2.UP * _wander_distance, _wander_radius, 0, 2 * PI, 360, Color(0, 0, 1, 1));
+			draw_circle(_wander_target, 5, Color(0, 1, 0, 1));
+		
+		if _type == SteeringBehaviorType.OBSTACLE_AVOIDANCE:
+			var _r = Rect2(Vector2(-_vehicle.width, _vehicle.height / 2 - _vehicle.height / 6), Vector2(_vehicle.width * 2, -_detection_box_length));
+			draw_rect(_r, Color.FUCHSIA, false, 1, false);
 
 
 func _process(_delta: float) -> void:
@@ -239,6 +295,9 @@ func calculate() -> Vector2:
 
 		SteeringBehaviorType.WANDER:
 			return _wander();
+
+		SteeringBehaviorType.OBSTACLE_AVOIDANCE:
+			return _obstacle_avoidance();
 
 		_:
 			return Vector2.ZERO;
@@ -311,3 +370,56 @@ func _wander() -> Vector2:
 	_wander_target += Vector2.UP * _wander_distance;
 
 	return to_global(_wander_target) - _vehicle.position;
+
+
+func _obstacle_avoidance() -> Vector2:
+	_detection_box_length = _min_detection_box_length + (_vehicle.velocity.length() / _vehicle.max_speed) * _min_detection_box_length;
+	_rectangle_shape2d.size = Vector2(_vehicle.width * 2, _detection_box_length);
+	_area2d.position = Vector2(0, -_detection_box_length / 2 + _vehicle.height / 2 - _vehicle.height / 6);
+	
+	var _dist_to_closest_ip = INF;
+	var _closest_obstacle: Obstacle = null;
+	var _local_position_obstacle: Vector2;
+	
+	for o in _obstacles:
+		#var _local_position = _vehicle.to_local(o.global_position);
+		var _local_position = o.global_position - _vehicle.global_position;
+		
+		if _local_position.x >= 0:
+			var _expanded_radius = o.radius + _vehicle.width;
+			
+			if abs(_local_position.y) < _expanded_radius:
+				var _cx = _local_position.x;
+				var _cy = _local_position.y;
+				var _sqrt = sqrt(_expanded_radius * _expanded_radius - _cy * _cy);
+				
+				var _ip = _cx - _sqrt;
+				if _ip <= 0:
+					_ip = _cx + _sqrt;
+					
+				if _ip < _dist_to_closest_ip:
+					_dist_to_closest_ip = _ip;
+					_closest_obstacle = o;
+					_local_position_obstacle = _local_position;
+	
+	if _closest_obstacle:
+		var _steering_force = Vector2.ZERO;
+		var multiplier = 1.0 + (_detection_box_length - _local_position_obstacle.x) / _detection_box_length;
+		_steering_force.y = (_closest_obstacle.radius - _local_position_obstacle.y) * multiplier;
+		
+		var _breaking_weight = .2;
+		_steering_force.x = (_closest_obstacle.radius - _local_position_obstacle.x) * _breaking_weight;
+		
+		return _vehicle.to_global(_steering_force);
+	
+	return Vector2.ZERO;
+
+
+func _obstacle_entered(area: Area2D):
+	if area.get_parent() is Obstacle:
+		_obstacles.append(area.get_parent());
+
+
+func _obstacle_exited(area: Area2D):
+	if area.get_parent() is Obstacle:
+		_obstacles.remove_at(_obstacles.find(area.get_parent()));
